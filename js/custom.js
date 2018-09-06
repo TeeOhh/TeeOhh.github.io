9707fccc75114398caf5fdd31ac9a203a11605ff
teeohh <taylorolson50@yahoo.com>
2018-09-06 11:15:07 -0500

    increased slider speed

--- js/custom.js
+++ js/custom.js
@@ -117,7 +117,7 @@
                 thumbs: false,
                 nav:false,
                 autoplay: true,
-                autoplayTimeout:4000
+                autoplayTimeout:1000000
                 smartSpeed: 1000,
                 dotsSpeed: 600,
             })
